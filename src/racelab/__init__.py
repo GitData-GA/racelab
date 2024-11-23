be9921bef4eff0d12afcb68b323027e215a59c51
Lee <120439693+lygitdata@users.noreply.github.com>
2024-11-23 10:07:40 +0100

    Update __init__.py

--- src/racelab/__init__.py
+++ src/racelab/__init__.py
@@ -2,7 +2,6 @@
 from racelab import utils
 from racelab import optimizer
 from racelab import deepracer
-from racelab import f1tenth
 from .load import load
 from .plot import plot
 from .optimize import optimize
