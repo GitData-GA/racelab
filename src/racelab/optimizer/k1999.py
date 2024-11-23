c2e16ab9177b533841a23ef9d4ffb6abb408d5f7
Lee <120439693+lygitdata@users.noreply.github.com>
2024-11-23 10:08:51 +0100

    Update k1999.py

--- src/racelab/optimizer/k1999.py
+++ src/racelab/optimizer/k1999.py
@@ -1,4 +1,5 @@
 from shapely.geometry import Point, Polygon
+from tqdm import tqdm
 from racelab.optimizer.utils.k1999 import menger_curvature, refine_point, refine_line
 
 
@@ -9,17 +10,16 @@ def k1999(track, line_iterations, xi_iterations, margin=0, atol=1e-3):
     Parameters
     ----------
     track : numpy.ndarray
-        A 2D array where each row represents waypoints on the track. Columns are 
-        structured in pairs as (x, y) coordinates, with the outer border first 
-        and the inner border last.
+        A 2D array where each row represents waypoints on the track.
+        Columns are structured in pairs as (x, y) coordinates, with the
+        outer border first and the inner border last.
     line_iterations : int
         The number of iterations to refine the racing line for optimality.
     xi_iterations : int
         The number of iterations to refine individual waypoints along the racing line.
     margin : float, optional
         A scaling factor to offset the inner and outer borders towards the middle of the track.
-        Positive values pull the edges inward, creating a buffer zone that prevents the
-        optimal line from hugging the track boundaries too closely. Defaults to 0 (no margin).
+        Defaults to 0 (no margin).
     atol : float, optional
         The absolute tolerance for convergence during refinement. Defaults to 1e-3.
 
@@ -27,19 +27,6 @@ def k1999(track, line_iterations, xi_iterations, margin=0, atol=1e-3):
     -------
     numpy.ndarray
         A 2D array representing the optimized racing line, with each row as (x, y) coordinates.
-
-    Notes
-    -----
-    - The algorithm starts with the middle line calculated as the average of the inner 
-      and outer borders.
-    - Margins are applied by adjusting the inner and outer borders towards the middle line.
-    - The refinement process ensures the racing line lies within the adjusted borders 
-      while optimizing for smoother curvature.
-    
-    References:
-    - Coulom, Rémi. "Apprentissage Par Renforcement Utilisant Des Réseaux de Neurones, 
-      Avec Des Applications Au Contrôle Moteur." Theses.hal.science, 19 June 2002, 
-      theses.hal.science/tel-00004386. Accessed 20 Feb. 2023.
     """
     if margin < 0:
         raise ValueError(
@@ -54,10 +41,11 @@ def k1999(track, line_iterations, xi_iterations, margin=0, atol=1e-3):
     inner_polygon = Polygon(inner_border + margin * (refined_line - inner_border))
     outer_polygon = Polygon(outer_border - margin * (outer_border - refined_line))
 
-    # Perform the line iterations
-    for _ in range(line_iterations):
-        refined_line = refine_line(
-            track, refined_line, inner_polygon, outer_polygon, xi_iterations, atol
-        )
+    with tqdm(total=line_iterations, desc="Optimizing racing line") as pbar:
+        for _ in range(line_iterations):
+            refined_line = refine_line(
+                track, refined_line, inner_polygon, outer_polygon, xi_iterations, atol
+            )
+            pbar.update(1)
 
     return refined_line
